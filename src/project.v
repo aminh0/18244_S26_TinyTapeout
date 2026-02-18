9d045da336fd1814d36f253f16c370599b50b632
aminh0 <hongamin@gmail.com>
2026-02-17 20:36:30 -0500

    test

--- src/project.v
+++ src/project.v
@@ -16,12 +16,26 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    logic [7:0] range_out;
+    logic debug_error;
+    logic [7:0] high_q, low_q;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    RangeFinder #(.WIDTH(8)) rf (
+        .data_in    (ui_in),
+        .clock      (clk),
+        .reset      (~rst_n),
+        .go         (uio_in[0]),
+        .finish     (uio_in[1]),
+        .range      (range_out),
+        .debug_error(debug_error),
+        .high_q     (high_q),
+        .low_q      (low_q)
+    );
+
+    assign uo_out  = range_out;
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
+
+    wire _unused = &{ena, debug_error, high_q, low_q, uio_in[7:2], 1'b0};
 
 endmodule
